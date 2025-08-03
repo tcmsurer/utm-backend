7c54d7c1cb0957f6461079ecd2cd59ab483e0c42
mnsurer <mnsurer@gmail.com>
2025-08-03 03:37:35 +0300

    AdD ADMN login

--- src/main/java/com/example/utm/service/JwtService.java
+++ src/main/java/com/example/utm/service/JwtService.java
@@ -6,14 +6,17 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
@@ -21,7 +24,6 @@ public class JwtService {
   private final String secretKey;
   private final long expirationTime;
 
-  // Değerleri constructor üzerinden alıyoruz
   public JwtService(@Value("${jwt.secret-key}") String secretKey,
       @Value("${jwt.expiration-time-ms}") long expirationTime) {
     this.secretKey = secretKey;
@@ -38,7 +40,14 @@ public class JwtService {
   }
 
   public String generateToken(UserDetails userDetails) {
-    return generateToken(new HashMap<>(), userDetails);
+    Map<String, Object> extraClaims = new HashMap<>();
+    List<String> roles = userDetails.getAuthorities()
+        .stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.toList());
+    extraClaims.put("authorities", roles);
+
+    return generateToken(extraClaims, userDetails);
   }
 
   public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
